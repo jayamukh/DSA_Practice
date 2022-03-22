8b6ab65a7ac21b0e125f3a44cf7638f31b9129d9
jayamukh <jayamukherjee.csus@gmail.com>
2022-03-22 21:58:29 +0530

    Time: 3 ms (96.85%), Space: 47.3 MB (67.78%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+        
+        for(int i= 0; i< graph.length; i++)
+        {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for(int[] edge:prerequisites)
+        {
+            int a= edge[0];
+            int b= edge[1];
+            graph[b].add(a);
+        }
+        
+        int[] vis = new int[numCourses];
+        //ArrayList<Intetger> topo  = new ArrayList<>();
+        
+        for(int i=0; i<vis.length; i++)
+        {
+            if(vis[i] == 0) 
+            {
+                boolean cycle = dfs(graph, i, vis);
+                if(cycle) return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean dfs(ArrayList<Integer>[] graph, int src, int[] vis){
+    {
+          vis[src]=1;
+        
+        for(int nbr:graph[src]){
+            if(vis[nbr]==0){
+                boolean cycle=dfs(graph,nbr,vis);
+                if(cycle) return true;
+            } else if(vis[nbr]==1){
+                return true;
+            }
+        }
+        
+        vis[src]=2;
+        return false;
+    }
+}
+}
