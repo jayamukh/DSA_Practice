3384174b790ec75df7b4a282bb2f5701c59ab550
jayamukh <jayamukherjee.csus@gmail.com>
2022-04-26 13:40:02 +0530

    Time: 3 ms (53.73%), Space: 45.3 MB (74.37%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -34,14 +34,45 @@ public int rec(int i, int j, int n, int m, int[][] grid){
         return memo[i][j]=ans+grid[i][j];
     }
     
+    public int tab( int n, int m, int[][] grid)
+    {
+        int[][] dp  = new int[n][m];
+        
+        for (int i = n-1; i>= 0; i--)
+        {
+            for(int j = m-1; j>= 0; j--)
+            {
+                if(i==n-1 && j==m-1)
+                {
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
+                
+                int ans=(int)(1e8);
+        
+        if(i+1<n)
+            ans=Math.min(ans,dp[i+1][j]);
+        
+        if(j+1<m)
+            ans=Math.min(ans,dp[i][j+1]);
+        
+        dp[i][j]=ans+grid[i][j];
+            }
+        }
+        
+        return dp[0][0];
+        
+    }
+    
     public int minPathSum(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
         
         int[][] memo=new int[n][m];
         
-        return rec_memo(0,0,n,m,grid,memo);
+        //return rec_memo(0,0,n,m,grid,memo);
         
         // return rec(0,0,n,m,grid);
+        return tab(n,m,grid);
     }
 }
