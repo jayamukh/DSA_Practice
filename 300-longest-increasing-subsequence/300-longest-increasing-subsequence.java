17b083dff2db19c7b20863da3202b306737e0101
jayamukh <jayamukherjee.csus@gmail.com>
2022-04-28 22:12:54 +0530

    Time: 4 ms (91.52%), Space: 41.6 MB (95.36%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,5 +1,43 @@
 class Solution {
-     public int lengthOfLIS(int[] nums) {
+    
+     public int BS(ArrayList<Integer> dp, int ele){
+        int si=0;
+        int ei=dp.size()-1;
+        
+        while(si<=ei){
+            int mid=(si+ei)/2;
+            
+            if(dp.get(mid)<ele){
+                si=mid+1;
+            } else {
+                ei=mid-1;
+            }
+        }
+        
+        return si;
+    }
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        
+        ArrayList<Integer> dp=new ArrayList<>();
+        
+        for(int i=0; i<n; i++){
+            int ele=nums[i];
+            
+            int pos=BS(dp,ele);
+            
+            if(pos==dp.size()){
+                dp.add(ele);
+            } else {
+                dp.set(pos,ele);
+            }
+        }
+        
+        return dp.size();
+    }
+    
+    
+     /*public int lengthOfLIS(int[] nums) {
         int n=nums.length;
         
         int[] dp=new int[n];
@@ -23,5 +61,5 @@ class Solution {
 //         }
         
         return lis;
-    }
+    }*/
 }
