4a54ce8f40de0cde598fab274c25a1538bb42dcd
jayamukh <jayamukherjee.csus@gmail.com>
2022-06-09 22:03:46 +0530

    Time: 28 ms (39.32%), Space: 51.8 MB (54.09%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
@@ -9,51 +9,81 @@
  */
 public class Codec {
 
-    public String recurSerial(TreeNode root, String str)
-    {
-        if (root ==  null)
-        {
-            str += "null,";
-        }
-        else
-        {
-            str += String.valueOf(root.val) + ",";
-            str = recurSerial(root.left, str);
-            str = recurSerial(root.right, str);
-        }
-        
-        return str;
-    }
-    
-    // Encodes a tree to a single string.
-    public String serialize(TreeNode root) {
-        return recurSerial(root, "");
-    }
 
-    // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-        String[] data_arr = data.split(",");
-        List<String> data_list = new LinkedList<String>(Arrays.asList(data_arr));
-        return recurDeserial(data_list);
-    }
-    
-    public TreeNode recurDeserial(List<String> l)
-    {
-        if(l.get(0).equals("null"))
-        {
-            l.remove(0);
-            return null;
-        }
-        TreeNode root  = new TreeNode(Integer.valueOf(l.get(0)));
-        l.remove(0);
-        root.left = recurDeserial(l);
-        root.right = recurDeserial(l);
-        
-        return root;
-    }
-}
 
 // Your Codec object will be instantiated and called as such:
 // Codec ser = new Codec();
 // Codec deser = new Codec();
 // TreeNode ans = deser.deserialize(ser.serialize(root));
+
+
+// Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) return "";
+        String n = "null", sep = ",";
+        Queue<TreeNode> dq = new LinkedList<>();
+        dq.offer(root);
+        int size = 0;
+        TreeNode cur;
+        StringBuilder sb = new StringBuilder();
+        
+        while (!dq.isEmpty()) {
+            size = dq.size();
+            for (int i = 0; i < size; ++i) {
+                cur = dq.poll();
+                if (cur != null) {
+                    sb.append(cur.val);
+                    dq.offer(cur.left);
+                    dq.offer(cur.right);
+                } else {
+                    sb.append(n);
+                }
+                sb.append(sep);
+            }
+        }
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == null || data.length() == 0) return null;
+        String[] vals = data.split(",");
+        if (vals == null || vals.length == 0) return null;
+        
+        String n = "null";
+        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+        TreeNode cur, next;
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        int size = 0, index = 1;
+        dq.offer(root);
+        
+        while (!dq.isEmpty()) {
+            size = dq.size();
+            for (int i = 0; i < size; ++i) {
+                cur = dq.poll();
+                
+                for (int j = index; j < index + 2 && j < vals.length; ++j) {
+                    if (vals[j].equals(n)) {
+                        if (j % 2 == 1) {
+                            cur.left = null;
+                        } else {
+                            cur.right = null;
+                        }
+                    } else {
+                        next = new TreeNode(Integer.parseInt(vals[j]));
+                        dq.offer(next);
+                        if (j % 2 == 1) {
+                            cur.left = next;
+                        } else {
+                            cur.right = next;
+                        }
+                    }
+                }
+                
+                index += 2;
+            }
+        }
+        
+        return root;
+    }
+}
