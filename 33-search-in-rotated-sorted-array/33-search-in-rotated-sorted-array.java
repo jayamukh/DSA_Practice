65f2c0147c2c245d9ebfb3aad84e767c63999cbf
jayamukh <jayamukherjee.csus@gmail.com>
2022-07-17 21:17:33 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (78.15%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,26 +1,38 @@
 class Solution {
-    public int search(int[] arr, int target) {
-        int si=0;
-        int ei=arr.length-1;
+    public int search(int[] nums, int target) {
         
-        while(si<=ei){
-            int mid=(si+ei)/2;
+        int si = 0;
+        int ei = nums.length - 1;
+        
+        while(ei >= si)
+        {
+            int mid = (si + ei)/2;
             
-            if(arr[mid]==target) return mid;
-            
-            if(arr[si]<=arr[mid]){ // region from si to mid is sorted
-                if(arr[si]<=target && arr[mid]>target){
-                    ei=mid-1;
-                } else {
-                    si=mid+1;
+            if(nums[mid] == target)
+                return mid;
+            if(nums[si] <= nums[mid])// left side sorted
+            {
+                if(target >= nums[si] && target < nums[mid])
+                {
+                    ei = mid -1;
                 }
-            } else {
-                if(arr[mid]<target && arr[ei]>=target){
-                    si=mid+1;
-                } else {
-                    ei=mid-1;
+                else
+                {
+                    si = mid + 1;
                 }
             }
+            else
+            {
+                if(target > nums[mid] && target <= nums[ei])
+                {
+                    si = mid +1;
+                }
+                else
+                {
+                    ei = mid -1;
+                }
+            }
+            
         }
         
         return -1;
